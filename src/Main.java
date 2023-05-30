606510604366e3dbe1f74b4ed4e2ec17429512c0
Olesya <lesya.garsiya@gmail.com>
2023-05-31 00:24:32 +0500

    на сдачу

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,8 @@ public class Main {
     public static void main(String[] args) {
         task1();
         task2();
+        task3();
+        task4();
     }
 
     public static int[] generateRandomArray() {
@@ -19,7 +21,7 @@ public class Main {
         int[] wastes = generateRandomArray();
         int sum = 0;
         for (int element : wastes) {
-            sum = sum + element;
+            sum += element;
         }
         System.out.println("Сумма трат за месяц составила " + sum + " рублей");
     }
@@ -42,5 +44,25 @@ public class Main {
         }
         System.out.println("Минимальная сумма трат за день составила " + minWastes + " рублей. Максимальная сумма" +
                 " трат за день составила " + maxWastes + " рублей");
+    }
+
+    public static void task3() {
+        System.out.println("Task 3");
+
+        int[] wastes = generateRandomArray();
+        double average = 0;
+        for (int element : wastes) {
+            average += element / 30;
+        }
+        System.out.println("Средняя сумма трат за месяц составила " + average + " рублей");
+    }
+
+    public static void task4() {
+        System.out.println("Task 4");
+
+        char[] reverseFullName = { 'n', 'a', 'v', 'I', ' ', 'v', 'o', 'n', 'a', 'v', 'I'};
+        for (int i = 10; i >= 0; i--) {
+            System.out.print(reverseFullName[i]);
+        }
     }
 }
